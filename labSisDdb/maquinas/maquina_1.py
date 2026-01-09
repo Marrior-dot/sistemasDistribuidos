88c9b299f0fc63628871432bfe12a131fc630f32
Marrior-dot <mrlimaroberto@gmail.com>
2026-01-09 09:08:42 -0300

    sockets se conectando, necessário testar a conexão com o banco de dados e o papel de coordenador

--- labSisDdb/maquinas/maquina_1.py
+++ labSisDdb/maquinas/maquina_1.py
@@ -1,19 +1,17 @@
 # first of all import the socket library 
 #from middleware import middleware_connection
 from node import Node
+from addr import listaNos
+import asyncio
 # next create a socket object 
-def main():
+async def main():
   node_id = 0
   socketHost = 'localhost'
   socketPort = 3000     
   dbHost = 'localhost'
   dbPort = 3306
-  outrasMaquinas:list = [
-     {"host":'localhost', 
-      "port":3001},
-      {"host":'localhost', 
-      "port":3002}]
-  maquina_1 = Node(node_id,socketHost,socketPort,dbHost,dbPort, outrasMaquinas)
+  maquina_1 = Node(node_id,socketHost,socketPort,dbHost,dbPort, listaNos)
   print ("Socket successfully created")
+  await maquina_1.requisicoesServidor(listaNos)
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
